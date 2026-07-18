abe448ab614703d855254f24b0eb35f06ca9de3b
Pathan Ismailkhan <129515476+Smile-Khan@users.noreply.github.com>
2026-07-18 18:52:01 +0530

    Add: search-for-word - submission-4

---
+++ Data Structures & Algorithms/search-for-word/submission-4.java
@@ -0,0 +1,50 @@
+class Solution {
+    private int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    public boolean exist(char[][] board, String word) {
+        int rows = board.length;
+        int cols = board[0].length;
+
+        boolean[][] visited = new boolean[rows][cols];
+
+        for(int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < cols; j++)
+            {
+                if(dfs(board, word, visited, i, j, 0))
+                {
+                    return true;
+                }
+            }
+        }    
+        return false;
+    }
+    private boolean dfs(char[][] board, String word, boolean[][] visited, int i, int j, int index)
+    {
+        if(index == word.length())
+        {
+            return true;
+        }
+
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || 
+        visited[i][j] || board[i][j] != word.charAt(index))
+        {
+            return false;
+        }
+        visited[i][j] = true;
+
+        for(int[] dir : dirs)
+        {
+            int newI = i + dir[0];
+            int newJ = j + dir[1];
+
+            if(dfs(board, word, visited, newI, newJ, index + 1))
+            {
+                return true;
+            }
+        }
+        visited[i][j] = false;
+
+        return false;
+    }
+}
